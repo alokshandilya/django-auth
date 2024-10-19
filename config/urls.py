65fc695eef9e71171944bda8d075814f098022e4
alokshandilya <alok.shandilya10@gmail.com>
2024-10-19 10:29:06 +0530

    fix(config/urls.py): add index and registration_view to urlpatterns
    make "/" and "/accounts/register/" point to index and registration_view
    respectively

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from .views import index, registration_view
+
 urlpatterns = [
+    path("", index, name="index"),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/register/", registration_view, name="register"),
 ]
